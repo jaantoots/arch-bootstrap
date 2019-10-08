073d9ab1f76c2e5e787a74239001e857ab1d6d83
Jaan Toots <jaan@jaantoots.org>
2019-10-08 22:52:53 +0300

    Update network configuration

--- archlive.sh
+++ archlive.sh
@@ -5,7 +5,7 @@ set -eux
 cp -av /usr/share/archiso/configs/releng/ archlive
 
 install -Dm644 -t archlive/airootfs/etc vconsole.conf
-install -Dm644 -t archlive/airootfs/etc/systemd/network 90-dhcp.network
+install -Dm644 -t archlive/airootfs/etc/systemd/network *.network
 install -Dm644 -t archlive/airootfs/etc/iwd iwd/main.conf
 install -Dm600 -t archlive/airootfs/var/lib/iwd iwd/*.psk
 install -Dm644 -t archlive/airootfs/root/.ssh authorized_keys
